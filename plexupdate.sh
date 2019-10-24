c1d0073777727354eb38d961981e0e68b6bbb814
nitantsoni <nitantsoni@gmail.com>
2019-10-23 21:26:47 -0700

    Update plexupdate.sh

--- plexupdate.sh
+++ plexupdate.sh
@@ -4,33 +4,34 @@
 #
 # Must be run as root.
 #
-# @author @martinorob https://github.com/martinorob
-# https://github.com/martinorob/plexupdate/
+# @source @martinorob https://github.com/martinorob/plexupdate
+# @author @nitantsoni https://github.com/nitantsoni/
 
-mkdir /volume1/plextemp/ > /dev/null 2>&1
-token=$(cat /volume1/Plex/Library/Application\ Support/Plex\ Media\ Server/Preferences.xml | grep -oP 'PlexOnlineToken="\K[^"]+')
-url=$(echo "https://plex.tv/api/downloads/5.json?channel=plexpass&X-Plex-Token=$token")
+mkdir -p /tmp/plex/
+cd /tmp/plex/
+
+url=$(echo "https://plex.tv/api/downloads/5.json")
 jq=$(curl -s ${url})
+
 newversion=$(echo $jq | jq -r .nas.Synology.version)
-echo New Ver: $newversion
+echo "Latest version: $newversion"
 curversion=$(synopkg version "Plex Media Server")
-echo Cur Ver: $curversion
+echo "Current version: $curversion"
 if [ "$newversion" != "$curversion" ]
-then
-echo New Vers Available
-/usr/syno/bin/synonotify PKGHasUpgrade '{"[%HOSTNAME%]": $(hostname), "[%OSNAME%]": "Synology", "[%PKG_HAS_UPDATE%]": "Plex", "[%COMPANY_NAME%]": "Synology"}'
-cpu=$(uname -m)
-if [ "$cpu" = "x86_64" ]; then
-url=$(echo $jq | jq -r ".nas.Synology.releases[1] | .url")
+    then
+    echo "Updated Version of Plex is available. Starting download & install."
+    /usr/syno/bin/synonotify PKGHasUpgrade '{"[%HOSTNAME%]": $(hostname), "[%OSNAME%]": "Synology", "[%PKG_HAS_UPDATE%]": "Plex", "[%COMPANY_NAME%]": "Synology"}'
+    cpu=$(uname -m)
+    if [ "$cpu" = "x86_64" ]; 
+        then
+        url=$(echo $jq | jq -r ".nas.Synology.releases[1] | .url")
+    else
+        url=$(echo $jq | jq -r ".nas.Synology.releases[0] | .url")
+    fi
+
+    /bin/wget $url -P /tmp/plex/
+    /usr/syno/bin/synopkg install /tmp/plex/*.spk && /usr/syno/bin/synopkg start "Plex Media Server" && rm -rf /tmp/plex/
 else
- url=$(echo $jq | jq -r ".nas.Synology.releases[0] | .url")
-fi
-/bin/wget $url -P /volume1/tmp/plex/
-/usr/syno/bin/synopkg install /volume1/tmp/plex/*.spk
-sleep 30
-/usr/syno/bin/synopkg start "Plex Media Server"
-rm -rf /volume1/tmp/plex/*
-else
-echo No New Ver
+    echo "No new version available"
 fi
 exit
